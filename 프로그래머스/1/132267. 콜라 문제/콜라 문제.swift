56f63b9826525bef8291c619bb3d5420915be57b
HyunRyeol Park <devpark435@gmail.com>
2024-03-06 14:55:01 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-    var emptyBottle = n
-    var result = 0
-
-    while emptyBottle >= a{
-        result += (emptyBottle / a) * b
-        emptyBottle = emptyBottle - (emptyBottle / a * a) + ((emptyBottle / a) * b)
-    }
-    return result
-
+    return 0
 }
