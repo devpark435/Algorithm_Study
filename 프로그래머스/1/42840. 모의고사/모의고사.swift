16b66ec08458f1a7c41b659f5b7f859cda69726b
HyunRyeol Park <devpark435@gmail.com>
2024-03-11 09:54:09 +0900

    [level 1] Title: 모의고사, Time: 4.85 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -1,22 +1,19 @@
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
-    let patterns = [[1, 2, 3, 4, 5],
-                    [2, 1, 2, 3, 2, 4, 2, 5],
-                    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    let patternLengths = patterns.map(\.count)
+    let answer = (
+        a: [1, 2, 3, 4, 5], 
+        b: [2, 1, 2, 3, 2, 4, 2, 5],
+        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
+    )
+    var point = [1:0, 2:0, 3:0]
 
-    var scores = [Int: Int]()
-
-    for i in 0..<answers.count {
-        for (student, pattern) in patterns.enumerated() {
-            let index = i % patternLengths[student]
-            if pattern[index] == answers[i] {
-                scores[student + 1] = (scores[student + 1] ?? 0) + 1
-            }
-        }
+    for (i, v) in answers.enumerated() {
+        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
+        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
+        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
     }
 
-    let maxScore = scores.values.max()!
-    return scores.filter { $0.value == maxScore }.sorted { $0.key < $1.key }.map { $0.key }
+    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+
 }
